91f305da81cf04a106b2d456c46d77450cb7893a
GIT-DS <darrickshin@gmail.com>
2021-11-22 14:43:50 -0800

    Add backend validation

---
+++ validation/landHolding.js
@@ -0,0 +1,49 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateAccountInput(data) {
+  let errors = {};
+
+  data.name = validText(data.name) ? data.name : '';
+  data.entityType = validText(data.entityType) ? data.entityType : '';
+  data.ownerType = validText(data.ownerType) ? data.ownerType : '';
+  data.address = validText(data.address) ? data.address : '';
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Account Name field is required';
+  }
+
+  if (!Validator.equals(data.entityType, "Company") && 
+    !Validator.equals(data.entityType, "Individual") && 
+    !Validator.equals(data.entityType, "Investor") && 
+    !Validator.equals(data.entityType, "Trust")
+  ) {
+    errors.entityType = 'Entity Type field is invalid';
+  }
+
+  if (!Validator.equals(data.ownerType, "Competitor") && 
+    !Validator.equals(data.ownerType, "Seller") && 
+    !Validator.equals(data.ownerType, "Investor") && 
+    !Validator.equals(data.ownerType, "Professional") 
+  
+  ) {
+    errors.ownerType = 'Owner Type field is invalid';
+  }
+
+  if (Validator.isEmpty(data.address)) {
+    errors.address = 'Address field is required';
+  }
+
+  if (Validator.isEmpty(data.numLandHoldings)){
+    errors.numLandHoldings = "Total Number of Land Holdings is required"
+  }
+
+  if (!Validator.isInt(data.numLandHoldings)){
+    errors.numLandHoldings = "Total Number of Land Holdings must be a number"
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
